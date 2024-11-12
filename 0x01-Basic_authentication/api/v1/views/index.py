f090b56cff72d72032fb8f9524e1829e6d772e25
Pury-01 <puryokumu@gmail.com>
2024-11-12 10:13:31 +0300

    Error handler: forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
         - the unauthorized error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """Get api/v1/forbidden
+    Return:
+        - the forbidden error
+    """
+    abort(403)
