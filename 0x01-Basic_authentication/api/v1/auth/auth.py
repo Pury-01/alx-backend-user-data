d5cf91afa8b2d6668dc63939d7254ea7af59e07e
Pury-01 <puryokumu@gmail.com>
2024-11-12 11:10:28 +0300

    Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,23 @@ class Auth():
     """manages  the API authentication.
     """
 
-    def require_auth(self, path: str, excluded_path: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns False."""
-        return False
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        normalized_path = path if path.endswith('/') else path + '/'
+
+        for excluded_path in excluded_paths:
+            normalized_excluded_path = excluded_path if\
+                    excluded_path.endswith('/') else excluded_path + '/'
+            if normalized_path == normalized_excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None. request are Flask request object.
@@ -21,3 +35,4 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None.
         """
+        return None
