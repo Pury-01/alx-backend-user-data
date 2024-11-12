fc7112de6bdf77d84f7445edfecfc6ba5c396d9d
Pury-01 <puryokumu@gmail.com>
2024-11-12 15:04:42 +0300

    fixed error and typos

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 AUTH_TYPE = getenv("AUTH_TYPE")
-if AUTH_TYPE == "BasicAuth":
+if AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
@@ -48,20 +48,20 @@ def forbidden(error) -> str:
 def before_request():
     """Enforce authorization rules.
     """
-    if Auth is None:
+    if auth is None:
         return
 
     public_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            'api/v1/forbidden/'
+            '/api/v1/forbidden/'
             ]
 
     if auth.require_auth(request.path, public_paths) is False:
         return
 
     if auth.authorization_header(request) is None:
-        abort(401)
+        abo<F3>rt(401)
 
     if auth.current_user(request) is None:
         abort(403)
