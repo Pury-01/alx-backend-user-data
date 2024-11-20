fb0739c9cf4c7602ca80d4b32c16da5ec3468f7e
Pury-01 <puryokumu@gmail.com>
2024-11-20 16:08:14 +0300

    basic flask application

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env  python3
+"""Flask Application
+"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def payload():
+    return jsonify({"message": "Bievenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
