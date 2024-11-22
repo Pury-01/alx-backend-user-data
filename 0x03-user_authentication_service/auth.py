43961e10d745dccd0a4e0f83c8ae28d45067f469
Pury-01 <puryokumu@gmail.com>
2024-11-22 18:38:00 +0300

    Reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -111,3 +111,18 @@ class Auth:
 
         except NoResultFound:
             pass
+
+    def get_reset_password_token(email: str) -> str:
+        """resets password token
+        """
+
+        try:
+            user = self._db.find_user_by(email=email)
+        except ValueError:
+            raise ValueError()
+
+        reset_token = _generate_uuid()
+
+        self._db.update_user(user.id, reset_token=reset_token)
+
+        return reset_token
